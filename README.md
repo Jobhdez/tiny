ae02dd8fe3b63c6e24d60be506b07d39baf6c863
Job Hernandez <jobhdezlara93@gmail.com>
2024-11-09 15:54:10 -0800

    add server

---
+++ README.md
@@ -0,0 +1,9 @@
+This a very tiny http server!
+
+### How to run the http server
+
+```
+$ python3 tiny.py
+```
+
+Now, point your browser to `http://127.0.0.1:65432/math.html`
---
+++ tiny.py
@@ -0,0 +1,69 @@
+import socket
+import re
+import os
+
+HOST = "127.0.0.1"
+PORT = 65432
+
+def handle_request(data):
+    method, uri, version = parse_get_request(data)
+
+    match method:
+        case 'GET':
+            path = uri.strip('/')
+            if not path:
+                path = 'index.html'
+
+            if os.path.exists(path) and not os.path.isdir(path):
+                response_line = 'HTTP/1.1 200 OK\r\n'
+                headers = ["Server: Manifold Server", "Content-Type: text/html\r\n"]
+                
+                with open(path, 'rb') as f:
+                    body = f.read()
+
+                header = "\r\n".join(headers).encode()
+                response_line = response_line.encode()
+                return b''.join([response_line, header, b'\r\n', body])
+            else:
+                response_line = 'HTTP/1.1 404 Not Found\r\n'
+                headers = ["Server: Manifold Server", "Content-Type: text/html\r\n"]
+                header = "\r\n".join(headers).encode()
+                body = b'<h1>404 Not Found</h1>'
+                response_line = response_line.encode()
+                return b''.join([response_line, header, b'\r\n', body])
+
+        case _:
+            response_line = 'HTTP/1.1 501 Not Implemented\r\n'
+            headers = ["Server: Manifold Server", "Content-Type: text/html\r\n"]
+            header = "\r\n".join(headers).encode()
+            body = b'<h1>501 Not Implemented</h1>'
+            response_line = response_line.encode()
+            return b''.join([response_line, header, b'\r\n', body])
+
+def parse_get_request(request):
+    pattern = re.compile(rb"^GET .+ HTTP/[0-9]*\.[0-9]+", re.MULTILINE)
+    request_line = pattern.findall(request)[0]
+
+    components = request_line.split(b' ')
+    method = components[0].decode()
+    uri = components[1].decode()
+    http_version = components[2].decode()
+    return method, uri, http_version
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print("Server is listening...")
+    
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            data = conn.recv(1024)
+            if not data:
+                break
+
+            response = handle_request(data)
+            print(f"Client says: {data.decode()}")
+            conn.sendall(response)
+        conn.close()
