953e50e4e5479476795b39f3695e3223a5a3a756
Job Hernandez <jobhdezlara93@gmail.com>
2024-11-10 14:03:15 -0800

    fix bug

--- tiny.py
+++ tiny.py
@@ -1,12 +1,16 @@
 import socket
 import re
 import os
-
+from urllib.parse import parse_qs
 HOST = "127.0.0.1"
 PORT = 65432
 
 def handle_request(data):
-    method, uri, version = parse_request(data)
+    try:
+        method, uri, version = parse_request(data)
+    except ValueError as e:
+        print(f"Error parsing request: {e}")
+        return build_response("400 Bad Request", "<h1>400 Bad Request</h1>")
 
     match method:
         case 'GET':
@@ -15,55 +19,63 @@ def handle_request(data):
                 path = 'index.html'
 
             if os.path.exists(path) and not os.path.isdir(path):
-                response_line = 'HTTP/1.1 200 OK\r\n'
-                headers = ["Server: Manifold Server", "Content-Type: text/html\r\n"]
-                
                 with open(path, 'rb') as f:
                     body = f.read()
+                return build_response("200 OK", body, "text/html")
 
-                header = "\r\n".join(headers).encode()
-                response_line = response_line.encode()
-                return b''.join([response_line, header, b'\r\n', body])
+            return build_response("404 Not Found", "<h1>404 Not Found</h1>")
 
-            response_line = 'HTTP/1.1 404 Not Found\r\n'
-            headers = ["Server: Manifold Server", "Content-Type: text/html\r\n"]
-            header = "\r\n".join(headers).encode()
-            body = b'<h1>404 Not Found</h1>'
-            response_line = response_line.encode()
-            return b''.join([response_line, header, b'\r\n', body])
-          
         case 'POST':
-          key_val_pattern = rb'(\w+)=([^&\s]+)'
-          data = re.findall(key_val_pattern, data)
-          val = int(data[0][1].decode())
-          val2 = int(data[1][1].decode())
+            print(data)
+            #key_val_pattern = rb'(\w+)=([^&\s]+)'
+            key_val_pattern = rb'(\w+)=([^&\s]+)'
 
-          body = f"<h1> the result is {val+val2}</h1>"
-
-          response_line = 'HTTP/1.1 200 OK\r\n'
-          headers = ["Server: Manifold Server", "Content-Type: text/html\r\n"]
-          header = "\r\n".join(headers).encode()
-          response_line = response_line.encode()
-          body = body.encode()
-          return b''.join([response_line, header, b'\r\n', body])
+            data = re.findall(key_val_pattern, data)
+            print(data)
+            try:
+                val = int(data[0][1].decode())
+                val2 = int(data[1][1].decode())
+                body = f"<h1>The result is {val + val2}</h1>"
+                return build_response("200 OK", body)
+            except (IndexError, ValueError):
+                return build_response("400 Bad Request", "<h1>400 Bad Request</h1>")
+            """body_data = data.split(b"\r\n\r\n", 1)[-1]
+            parsed_data = parse_qs(body_data.decode())
+            print(parsed_data)
+            try:
+              val1 = int(parsed_data.get("num1", [0])[0])
+              val2 = int(parsed_data.get("num2", [0])[0])
+              body = f"<h1>The result is {val1 + val2}</h1>"
+              return build_response("200 OK", body)
+            except (ValueError, TypeError):
+              return build_response("400 Bad Request", "<h1>400 Bad Request</h1>")"""
 
         case _:
-            response_line = 'HTTP/1.1 501 Not Implemented\r\n'
-            headers = ["Server: Manifold Server", "Content-Type: text/html\r\n"]
-            header = "\r\n".join(headers).encode()
-            body = b'<h1>501 Not Implemented</h1>'
-            response_line = response_line.encode()
-            return b''.join([response_line, header, b'\r\n', body])
+            return build_response("501 Not Implemented", "<h1>501 Not Implemented</h1>")
 
 def parse_request(request):
-    pattern = re.compile(rb"^GET|POST .+ HTTP/[0-9]*\.[0-9]+", re.MULTILINE)
-    request_line = pattern.findall(request)[0]
+    request_line_pattern = re.compile(rb"^(GET|POST) .+ HTTP/[0-9.]+")
+    match = request_line_pattern.search(request)
+    if not match:
+        raise ValueError("Invalid request line")
+    request_line = match.group().decode()
+    components = request_line.split(" ")
+    return components[0], components[1], components[2]
 
-    components = request_line.split(b' ')
-    method = components[0].decode()
-    uri = components[1].decode()
-    http_version = components[2].decode()
-    return method, uri, http_version
+def build_response(status, body, content_type="text/html"):
+    response_line = f"HTTP/1.1 {status}\r\n"
+    headers = [
+        "Server: Manifold Server",
+        f"Content-Type: {content_type}\r\n",
+        "Access-Control-Allow-Origin: *",  # This allows requests from any origin
+        "Access-Control-Allow-Methods: GET, POST",  # Allow GET and POST methods
+        "Access-Control-Allow-Headers: Content-Type"  # Allow content-type header
+    ]
+    header = "\r\n".join(headers).encode()
+    response_line = response_line.encode()
+    body = body.encode() if isinstance(body, str) else body
+    return b''.join([response_line, header, b'\r\n', body])
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -79,6 +91,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 break
 
             response = handle_request(data)
-            print(f"Client says: {data.decode()}")
+            print(f"Client says: {data.decode(errors='ignore')}")
             conn.sendall(response)
-        conn.close()
